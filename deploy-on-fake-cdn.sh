a4611a5948f057807aa89c034f5eb63eb65ba8ca
TDR <tdr@tdrs.ro>
2020-09-02 02:49:07 +0300

    updated "deploy" script

--- deploy-on-fake-cdn.sh
+++ deploy-on-fake-cdn.sh
@@ -13,13 +13,14 @@ tar -czf ../mcms-form.tgz ../../index.html ./*.js ./*.js.map ./*.css ./*.css.map
 
 rm ../../index.html || exit
 
-targetPath=/var/www/tdrs.ro/fake-cdn/
+targetPath=/var/www/tdrs.ro/fake-cdn
 
 echo "uploading ..."
-scp -i $keyPath ../mcms-form.tgz root@tdrs.ro:$targetPath/public_html || exit
+scp -i $keyPath ../mcms-form.tgz tdr@tdrs.ro:~/ || exit
+ssh -i $keyPath tdr@tdrs.ro "sudo mv ~/mcms-form.tgz $targetPath/public_html" || exit
 
 echo "deploying ..."
-ssh -i $keyPath root@tdrs.ro "$targetPath/deploy-mcms-form.sh" || exit
+ssh -i $keyPath tdr@tdrs.ro "sudo $targetPath/deploy-mcms-form.sh" || exit
 
 rm ../mcms-form.tgz
 
