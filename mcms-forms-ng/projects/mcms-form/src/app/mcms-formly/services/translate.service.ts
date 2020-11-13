a30a9ea9d23f2564e1a680a1ba12b691c47c9b29
TDR <tdr@tdrs.ro>
2020-11-13 05:29:53 +0200

    1.2.6: bypass security on basic field texts

--- mcms-forms-ng/projects/mcms-form/src/app/mcms-formly/services/translate.service.ts
+++ mcms-forms-ng/projects/mcms-form/src/app/mcms-formly/services/translate.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { IOpenApiDocument } from '../models/open-api-models';
 import { FormlyConfig, FormlyFieldConfig } from '@ngx-formly/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Injectable()
 export class TranslateService {
 
   private docValue: IOpenApiDocument;
+  private translationsDict: { [key: string]: string };
 
   public set doc(value: IOpenApiDocument) {
     this.docValue = value;
@@ -13,15 +15,19 @@ export class TranslateService {
 
   constructor(
     private formlyConfig: FormlyConfig,
+    private sanitizer: DomSanitizer,
   ) {
   }
 
   public getTranslationsDict(doc: IOpenApiDocument): { [key: string]: string } {
-    const lang = this.getLang() || doc.translations.defaultLanguage;
-    if (!doc || !doc.translations || !doc.translations.translations) {
-      return {};
+    if (!this.translationsDict) {
+      const lang = this.getLang() || doc.translations.defaultLanguage;
+      if (!doc || !doc.translations || !doc.translations.translations) {
+        return {};
+      }
+      this.translationsDict = doc.translations.translations[lang] || {};
     }
-    return doc.translations.translations[lang] || {};
+    return this.translationsDict;
   }
 
   public getLang(): string {
@@ -39,12 +45,27 @@ export class TranslateService {
 
   public tryTranslate(msg: string): string {
     const trans = this.getTranslationsDict(this.docValue);
-    return trans.hasOwnProperty(msg) ? trans[msg] : msg;
+    if (trans.hasOwnProperty(msg)) {
+      const value = trans[msg];
+      return value;
+    } else {
+      return msg;
+    }
+  }
+
+  public tryTranslateAndBypassSecurity(msg: string): string | SafeHtml {
+    const trans = this.getTranslationsDict(this.docValue);
+    if (trans.hasOwnProperty(msg)) {
+      const value = trans[msg];
+      return this.sanitizer.bypassSecurityTrustHtml(value);
+    } else {
+      return msg;
+    }
   }
 
   public translate(fieldConfig: FormlyFieldConfig): void {
-    fieldConfig.templateOptions.label = this.tryTranslate(fieldConfig.templateOptions.label);
-    fieldConfig.templateOptions.placeholder = this.tryTranslate(fieldConfig.templateOptions.placeholder);
-    fieldConfig.templateOptions.description = this.tryTranslate(fieldConfig.templateOptions.description);
+    fieldConfig.templateOptions.label = this.tryTranslateAndBypassSecurity(fieldConfig.templateOptions.label) as string;
+    fieldConfig.templateOptions.placeholder = this.tryTranslateAndBypassSecurity(fieldConfig.templateOptions.placeholder) as string;
+    fieldConfig.templateOptions.description = this.tryTranslateAndBypassSecurity(fieldConfig.templateOptions.description) as string;
   }
 }
