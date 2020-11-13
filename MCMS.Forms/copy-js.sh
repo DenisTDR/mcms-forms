e6c458ea2a5b605e9c815a151b2a023bc19f25f4
TDR <tdr@tdrs.ro>
2020-11-13 04:47:36 +0200

    1.2.4

--- MCMS.Forms/copy-js.sh
+++ MCMS.Forms/copy-js.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-cp ../mcms-forms-ng/dist/mcms-form/mcms-form.*.js ../mcms-forms-ng/dist/mcms-form/styles.css ../mcms-forms-ng/dist/mcms-form/3rdpartylicenses.txt ./resources/mcms-forms-files/ || exit
+resPath=./resources/mcms-forms-files/
+
+rm -rf ${resPath:?}*
+
+cp ../mcms-forms-ng/dist/mcms-form/mcms-form.*.js ../mcms-forms-ng/dist/mcms-form/styles.css \
+  ../mcms-forms-ng/dist/mcms-form/3rdpartylicenses.txt ${resPath:?} || exit
+  
+echo "copied: "
+  
+ls -lh ${resPath:?}
