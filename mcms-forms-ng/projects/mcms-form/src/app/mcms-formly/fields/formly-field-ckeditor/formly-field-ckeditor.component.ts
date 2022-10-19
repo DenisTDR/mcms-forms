88bc0a70496f1943e05378d3ed3a47fc25f0b6d4
TDR <tdr@tdrs.ro>
2022-10-19 22:57:59 +0300

    custom image upload url in ckeditor

--- mcms-forms-ng/projects/mcms-form/src/app/mcms-formly/fields/formly-field-ckeditor/formly-field-ckeditor.component.ts
+++ mcms-forms-ng/projects/mcms-form/src/app/mcms-formly/fields/formly-field-ckeditor/formly-field-ckeditor.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as BalloonEditorMcms from '@mcms-cs/ckeditor5-build-balloon';
 import { FieldType } from '@ngx-formly/core';
 
@@ -7,12 +7,19 @@ import { FieldType } from '@ngx-formly/core';
   templateUrl: './formly-field-ckeditor.component.html',
   styleUrls: ['./formly-field-ckeditor.component.scss'],
 })
-export class FormlyFieldCkeditorComponent extends FieldType {
+export class FormlyFieldCkeditorComponent extends FieldType implements OnInit {
   public Editor = BalloonEditorMcms;
   public CKEDITOR_CONFIG = {
     // toolbar: ['bold', 'italic', '|', 'undo', 'redo', '|', 'link', '|', 'NumberedList', 'BulletedList', '|', 'Outdent', 'Indent', '|'],
     // link: {
     //   addTargetToExternalLinks: true,
     // },
+    simpleUpload: {
+      uploadUrl: '/api/FilesUpload/UploadCkEditor',
+    },
   };
+
+  public ngOnInit(): void {
+    this.CKEDITOR_CONFIG.simpleUpload.uploadUrl = this.to?.customFieldConfig?.imageUploadUrl || this.CKEDITOR_CONFIG.simpleUpload.uploadUrl;
+  }
 }
