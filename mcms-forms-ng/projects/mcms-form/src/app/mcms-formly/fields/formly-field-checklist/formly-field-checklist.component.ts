7ae2dc4260a5acc7f7eb1527fc95c66db3d8b12c
TDR <tdr@tdrs.ro>
2023-07-05 19:48:19 +0300

    bypass security in checklist label html

--- mcms-forms-ng/projects/mcms-form/src/app/mcms-formly/fields/formly-field-checklist/formly-field-checklist.component.ts
+++ mcms-forms-ng/projects/mcms-form/src/app/mcms-formly/fields/formly-field-checklist/formly-field-checklist.component.ts
@@ -1,33 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FieldType } from '@ngx-formly/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @UntilDestroy()
 @Component({
   selector: 'app-formly-field-checklist',
   template: `
-      <ng-container *ngIf="checkListOptions">
-          <div [ngClass]="to.customFieldConfig?.optionClass || 'my-2'" *ngFor="let opt of checkListOptions">
-              <div class="custom-control custom-switch" (click)="input.click();">
-                  <input class="custom-control-input"
-                         type="checkbox"
-                         #input
-                         [checked]="checkedOptions[opt.value]"
-                         (click)="$event.stopPropagation()"
-                         (change)="toggleOption(opt.value)"
-                  >
-                  <label class="custom-control-label" [innerHTML]="opt.label"></label>
-              </div>
-          </div>
-      </ng-container>
+    <ng-container *ngIf="checkListOptions">
+      <div [ngClass]="to.customFieldConfig?.optionClass || 'my-2'" *ngFor="let opt of checkListOptions">
+        <div class="custom-control custom-switch" (click)="input.click();">
+          <input class="custom-control-input"
+                 type="checkbox"
+                 #input
+                 [checked]="checkedOptions[opt.value]"
+                 (click)="$event.stopPropagation()"
+                 (change)="toggleOption(opt.value)"
+          >
+          <label class="custom-control-label" [innerHTML]="sanitize(opt.label)"></label>
+        </div>
+      </div>
+    </ng-container>
   `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormlyFieldChecklistComponent extends FieldType implements OnInit {
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    private cd: ChangeDetectorRef,
+  ) {
+    super();
+  }
 
   public checkedOptions: Record<string, boolean> = {};
 
   public get checkListOptions(): { value: string, label: string }[] {
     return this.to.options as { value: string, label: string }[];
+  }
+
+  public sanitize(html): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(html);
   }
 
   public ngOnInit(): void {
@@ -53,6 +66,7 @@ export class FormlyFieldChecklistComponent extends FieldType implements OnInit {
         this.checkedOptions[key] = true;
       }
     }
+    this.cd.markForCheck();
   }
 
   private flattenValues(): string[] {
